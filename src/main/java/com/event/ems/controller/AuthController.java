1799f6a4f6e7ec963bb3fca5636259c366153f7e
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2026-01-16 20:15:54 +0530

    feat -: AuthController

---
+++ src/main/java/com/event/ems/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.event.ems.controller;
+
+import com.event.ems.dto.AuthResponse;
+import com.event.ems.dto.LoginRequest;
+import com.event.ems.dto.RegisterRequest;
+import com.event.ems.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.event.ems.entity.User;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+        userService.registerUser(request);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest request) {
+
+        return ResponseEntity.ok(userService.login(request));
+    }
+
+}
