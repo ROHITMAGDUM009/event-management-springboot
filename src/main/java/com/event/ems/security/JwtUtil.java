015e42c13bbc1beaf495f671a6e53f4e6704f491
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2026-01-16 20:16:54 +0530

    chore -: JwtUtil

---
+++ src/main/java/com/event/ems/security/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.event.ems.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY =
+            "ZW1zU2VjdXJlS2V5MTIzNDU2Nzg5MDEyMzQ1Njc4OTA=";
+
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
