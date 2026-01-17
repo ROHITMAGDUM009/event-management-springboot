6a4b3e0f00d108c1075fd74284fa16ae312d2aa4
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-17 22:39:35 +0530

    refactor -: EventRepository

--- src/main/java/com/event/ems/repository/EventRepository.java
+++ src/main/java/com/event/ems/repository/EventRepository.java
@@ -1,2 +1,11 @@
-package com.event.ems.repository;public interface EventRepository {
+package com.event.ems.repository;
+
+import com.event.ems.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByCreatedBy(String email);
 }
