176419439b8e3add1d5551c674e7e5ff54184ece
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-17 22:39:29 +0530

    update -: Event

--- src/main/java/com/event/ems/entity/Event.java
+++ src/main/java/com/event/ems/entity/Event.java
@@ -1,2 +1,31 @@
-package com.event.ems.entity;public class Event {
+package com.event.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String location;
+
+    private LocalDate eventDate;
+
+    // Who created this event
+    private String createdBy; // email from JWT
 }
