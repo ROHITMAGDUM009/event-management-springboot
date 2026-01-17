f3446e6443525b0ea37bfec4a445b9fdfb3dffe9
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-17 22:39:56 +0530

    update -: EventServiceImpl

--- src/main/java/com/event/ems/service/impl/EventServiceImpl.java
+++ src/main/java/com/event/ems/service/impl/EventServiceImpl.java
@@ -1,2 +1,60 @@
-package com.event.ems.service.impl;public class EventServiceImpl {
+package com.event.ems.service.impl;
+
+import com.event.ems.dto.EventRequest;
+import com.event.ems.entity.Event;
+import com.event.ems.repository.EventRepository;
+import com.event.ems.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+
+    @Override
+    public Event createEvent(EventRequest request, String email) {
+
+        Event event = Event.builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .location(request.getLocation())
+                .eventDate(request.getEventDate())
+                .createdBy(email)
+                .build();
+
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event updateEvent(Long id, EventRequest request, String email) {
+
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found"));
+
+        // Organizer can update ONLY own event
+        if (!event.getCreatedBy().equals(email)) {
+            throw new RuntimeException("Not allowed");
+        }
+
+        event.setTitle(request.getTitle());
+        event.setDescription(request.getDescription());
+        event.setLocation(request.getLocation());
+        event.setEventDate(request.getEventDate());
+
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
 }
