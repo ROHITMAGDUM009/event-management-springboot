ce40be62cb2a4834484a5df7449f10224d8216e1
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-17 22:39:51 +0530

    refactor -: EventService

--- src/main/java/com/event/ems/service/EventService.java
+++ src/main/java/com/event/ems/service/EventService.java
@@ -1,2 +1,17 @@
-package com.event.ems.service;public class EventService {
+package com.event.ems.service;
+
+import com.event.ems.dto.EventRequest;
+import com.event.ems.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+
+    Event createEvent(EventRequest request, String email);
+
+    List<Event> getAllEvents();
+
+    Event updateEvent(Long id, EventRequest request, String email);
+
+    void deleteEvent(Long id);
 }
