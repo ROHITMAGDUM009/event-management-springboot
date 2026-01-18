11e51f2514b24d9fde2ed894421f43d654158ee5
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-18 19:56:00 +0530

    refactor : AdminUserService

--- src/main/java/com/event/ems/service/AdminUserService.java
+++ src/main/java/com/event/ems/service/AdminUserService.java
@@ -1,2 +1,17 @@
-package com.event.ems.service;public class AdminUserService {
+package com.event.ems.service;
+
+import com.event.ems.dto.UserResponse;
+import java.util.List;
+
+public interface AdminUserService {
+
+    List<UserResponse> getAllUsers();
+
+    void changeUserStatus(Long userId, boolean enabled);
+
+    void makeOrganizer(Long userId);
+
+    void removeOrganizer(Long userId);
+
+    void deleteUser(Long userId);
 }
