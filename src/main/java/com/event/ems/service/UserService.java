9ee69daddb3785a02ae2becf366e0e4fa932a8fe
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-18 19:56:11 +0530

    refactor : UserService

--- src/main/java/com/event/ems/service/UserService.java
+++ src/main/java/com/event/ems/service/UserService.java
@@ -4,10 +4,13 @@ import com.event.ems.dto.AuthResponse;
 import com.event.ems.dto.LoginRequest;
 import com.event.ems.dto.RegisterRequest;
 import com.event.ems.entity.User;
+import com.event.ems.dto.UserResponse;
 
 public interface UserService {
 
     User registerUser(RegisterRequest request);
 
     AuthResponse login(LoginRequest request);
+
+    UserResponse getMyProfile(String email);
 }
