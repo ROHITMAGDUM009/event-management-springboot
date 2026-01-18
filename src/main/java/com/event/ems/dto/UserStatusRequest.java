a5a6c4f3c4f594cca497802c01a710ee1b7eec14
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-18 19:55:38 +0530

    update : UserStatusRequest

--- src/main/java/com/event/ems/dto/UserStatusRequest.java
+++ src/main/java/com/event/ems/dto/UserStatusRequest.java
@@ -1,2 +1,14 @@
-package com.event.ems.dto;public class UserStatusRequest {
+package com.event.ems.dto;
+
+public class UserStatusRequest {
+
+    private boolean enabled;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
