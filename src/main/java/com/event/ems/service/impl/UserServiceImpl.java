ae792587c2a2235f3a5ffc6a6443a1c889e1c4bb
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-18 19:56:27 +0530

    update : UserServiceImpl

--- src/main/java/com/event/ems/service/impl/UserServiceImpl.java
+++ src/main/java/com/event/ems/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.event.ems.service.impl;
 import com.event.ems.dto.AuthResponse;
 import com.event.ems.dto.LoginRequest;
 import com.event.ems.dto.RegisterRequest;
+import com.event.ems.dto.UserResponse;
 import com.event.ems.entity.Role;
 import com.event.ems.entity.RoleName;
 import com.event.ems.entity.User;
@@ -73,6 +74,26 @@ public class UserServiceImpl implements UserService {
         String token = jwtUtil.generateToken(user.getEmail(), role);
 
         return new AuthResponse(token, role);
+    }
+
+    @Override
+    public UserResponse getMyProfile(String email) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String role = user.getRoles()
+                .iterator()
+                .next()
+                .getName()
+                .name();
+
+        return new UserResponse(
+                user.getId(),
+                user.getFullName(),
+                user.getEmail(),
+                role
+        );
     }
 
 }
