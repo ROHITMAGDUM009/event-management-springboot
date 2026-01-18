9ae7c106feee338063edf7e30eba2df76a82d7b5
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-18 19:55:33 +0530

    update : UserResponse

--- src/main/java/com/event/ems/dto/UserResponse.java
+++ src/main/java/com/event/ems/dto/UserResponse.java
@@ -1,2 +1,61 @@
-package com.event.ems.dto;public class UserResponse {
+package com.event.ems.dto;
+
+public class UserResponse {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private boolean enabled;
+    private String role;
+
+    public UserResponse(Long id, String fullName, String email, String role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public UserResponse() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
