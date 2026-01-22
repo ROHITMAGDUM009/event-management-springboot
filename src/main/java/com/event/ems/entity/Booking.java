004dd545543c25a9757e0c17c289272a47dc29d5
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-22 16:46:57 +0530

    update : Booking

--- src/main/java/com/event/ems/entity/Booking.java
+++ src/main/java/com/event/ems/entity/Booking.java
@@ -1,2 +1,97 @@
-package com.event.ems.entity;public class Booking {
+package com.event.ems.entity;
+
+import com.event.ems.entity.BookingStatus;
+import com.event.ems.entity.PaymentStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long eventId;
+    private String eventTitle;
+    private String userEmail;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private LocalDateTime createdAt;
+
+
+    // getters & setters
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getEventId() {
+        return this.eventId;
+    }
+
+    public void setEventId(final Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventTitle() {
+        return this.eventTitle;
+    }
+
+    public void setEventTitle(final String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public String getUserEmail() {
+        return this.userEmail;
+    }
+
+    public void setUserEmail(final String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(final Double amount) {
+        this.amount = amount;
+    }
+
+    public BookingStatus getBookingStatus() {
+        return this.bookingStatus;
+    }
+
+    public void setBookingStatus(final BookingStatus bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return this.paymentStatus;
+    }
+
+    public void setPaymentStatus(final PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(final LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
