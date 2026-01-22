3800082e95e64d831dfb9b5d3d5c72cc8eebc931
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-22 16:46:51 +0530

    update : BookingRequest

--- src/main/java/com/event/ems/dto/BookingRequest.java
+++ src/main/java/com/event/ems/dto/BookingRequest.java
@@ -1,2 +1,14 @@
-package com.event.ems.dto;public class BookingRequest {
+package com.event.ems.dto;
+
+public class BookingRequest {
+
+    private Long eventId;
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
 }
