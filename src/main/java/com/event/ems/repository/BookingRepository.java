87d713bda864bb4d9dc60bf1ae42417242aefd9f
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-01-22 16:47:13 +0530

    refactor : BookingRepository

--- src/main/java/com/event/ems/repository/BookingRepository.java
+++ src/main/java/com/event/ems/repository/BookingRepository.java
@@ -1,2 +1,13 @@
-package com.event.ems.repository;public class BookingRepository {
+package com.event.ems.repository;
+
+import com.event.ems.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByUserEmail(String email);
+
+    List<Booking> findByEventId(Long eventId);
 }
