15656407af112b4167e1852595bc941ee6055e8d
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-02-06 11:29:10 +0530

    feat: OrganizerBookingController

--- src/main/java/com/event/ems/controller/OrganizerBookingController.java
+++ src/main/java/com/event/ems/controller/OrganizerBookingController.java
@@ -1,2 +1,24 @@
-package com.event.ems.controller;public class OrganizerBookingController {
+package com.event.ems.controller;
+
+import com.event.ems.entity.Booking;
+import com.event.ems.entity.BookingStatus;
+import com.event.ems.repository.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/organizer/bookings")
+@RequiredArgsConstructor
+public class OrganizerBookingController {
+
+    private final BookingRepository bookingRepository;
+
+    @PostMapping("/{id}/approve")
+    public Booking approveBooking(@PathVariable Long id) {
+        Booking booking = bookingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Booking not found"));
+
+        booking.setBookingStatus(BookingStatus.APPROVED);
+        return bookingRepository.save(booking);
+    }
 }
