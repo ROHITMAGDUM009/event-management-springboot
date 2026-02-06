689d96013ba3c32370b0f9405fabc1f870c3016c
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-02-06 11:29:07 +0530

    feat: BookingController

--- src/main/java/com/event/ems/controller/BookingController.java
+++ src/main/java/com/event/ems/controller/BookingController.java
@@ -4,7 +4,6 @@ import com.event.ems.dto.BookingRequest;
 import com.event.ems.entity.Booking;
 import com.event.ems.service.BookingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,33 +16,11 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    // USER
     @PostMapping
-    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Booking bookEvent(
             @RequestBody BookingRequest request,
             Authentication authentication
     ) {
-        return bookingService.bookEvent(request.getEventId(), authentication.getName());
-    }
-
-    // USER
-    @GetMapping("/my")
-    @PreAuthorize("hasAuthority('ROLE_USER')")
-    public List<Booking> myBookings(Authentication authentication) {
-        return bookingService.getUserBookings(authentication.getName());
-    }
-
-    // ORGANIZER
-    @PutMapping("/{id}/approve")
-    @PreAuthorize("hasAuthority('ROLE_ORGANIZER')")
-    public Booking approve(@PathVariable Long id) {
-        return bookingService.approveBooking(id);
-    }
-
-    @PutMapping("/{id}/reject")
-    @PreAuthorize("hasAuthority('ROLE_ORGANIZER')")
-    public Booking reject(@PathVariable Long id) {
-        return bookingService.rejectBooking(id);
+        return bookingService.createBooking(request, authentication.getName());
     }
 }
