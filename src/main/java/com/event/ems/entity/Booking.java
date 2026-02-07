8cd4597973a6b5fe1b3dc39695aefb8046369090
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-02-07 09:30:51 +0530

    update: Booking

--- src/main/java/com/event/ems/entity/Booking.java
+++ src/main/java/com/event/ems/entity/Booking.java
@@ -1,24 +1,26 @@
 package com.event.ems.entity;
 
-import com.event.ems.entity.BookingStatus;
-import com.event.ems.entity.PaymentStatus;
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
 @Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long eventId;
-    private String eventTitle;
     private String userEmail;
 
-    private Double amount;
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
 
     @Enumerated(EnumType.STRING)
     private BookingStatus bookingStatus;
@@ -26,72 +28,5 @@ public class Booking {
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
 
-    private LocalDateTime createdAt;
-
-
-    // getters & setters
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getEventId() {
-        return this.eventId;
-    }
-
-    public void setEventId(final Long eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getEventTitle() {
-        return this.eventTitle;
-    }
-
-    public void setEventTitle(final String eventTitle) {
-        this.eventTitle = eventTitle;
-    }
-
-    public String getUserEmail() {
-        return this.userEmail;
-    }
-
-    public void setUserEmail(final String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public Double getAmount() {
-        return this.amount;
-    }
-
-    public void setAmount(final Double amount) {
-        this.amount = amount;
-    }
-
-    public BookingStatus getBookingStatus() {
-        return this.bookingStatus;
-    }
-
-    public void setBookingStatus(final BookingStatus bookingStatus) {
-        this.bookingStatus = bookingStatus;
-    }
-
-    public PaymentStatus getPaymentStatus() {
-        return this.paymentStatus;
-    }
-
-    public void setPaymentStatus(final PaymentStatus paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return this.createdAt;
-    }
-
-    public void setCreatedAt(final LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+    private Double amount;
 }
