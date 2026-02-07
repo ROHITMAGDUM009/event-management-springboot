4a53969ae740beb3adb1e19ba9befda2929e9b33
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-02-07 09:30:54 +0530

    update: BookingApprovalType

--- src/main/java/com/event/ems/entity/BookingApprovalType.java
+++ src/main/java/com/event/ems/entity/BookingApprovalType.java
@@ -1,2 +1,6 @@
-package com.event.ems.entity;public enum BookingApprovalType {
+package com.event.ems.entity;
+
+public enum BookingApprovalType {
+    AUTO,      // No organizer approval
+    MANUAL    // Organizer approval required
 }
