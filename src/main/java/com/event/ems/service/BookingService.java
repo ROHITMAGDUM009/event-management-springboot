ef6f6dc221f7c65cecbc64c0ec0432d73d0409e8
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-02-08 21:10:27 +0530

    refactor: BookingService

--- src/main/java/com/event/ems/service/BookingService.java
+++ src/main/java/com/event/ems/service/BookingService.java
@@ -1,18 +1,10 @@
 package com.event.ems.service;
 
+import com.event.ems.dto.BookingRequest;
 import com.event.ems.entity.Booking;
-
-import java.util.List;
 
 public interface BookingService {
 
-    Booking bookEvent(Long eventId, String userEmail);
+    Booking createBooking(BookingRequest request, String userEmail);
 
-    List<Booking> getUserBookings(String email);
-
-    List<Booking> getBookingsByEvent(Long eventId);
-
-    Booking approveBooking(Long bookingId);
-
-    Booking rejectBooking(Long bookingId);
 }
