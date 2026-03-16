dd6fd3e238eada0eff858c0db092261c7af8c97f
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-03-16 10:18:28 +0530

    update: OrganizerDTO

--- src/main/java/com/event/ems/dto/OrganizerDTO.java
+++ src/main/java/com/event/ems/dto/OrganizerDTO.java
@@ -1,2 +1,21 @@
-package com.event.ems.dto;public class OrganizerDTO {
+package com.event.ems.dto;
+
+public class OrganizerDTO {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private boolean enabled;
+
+    public OrganizerDTO(Long id, String fullName, String email, boolean enabled) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.enabled = enabled;
+    }
+
+    public Long getId() { return id; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
+    public boolean isEnabled() { return enabled; }
 }
