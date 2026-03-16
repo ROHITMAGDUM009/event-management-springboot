a1815ffd3e242d4e54b9406af189e157be521936
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-03-16 10:18:24 +0530

    update: DashboardStatsDTO

--- src/main/java/com/event/ems/dto/DashboardStatsDTO.java
+++ src/main/java/com/event/ems/dto/DashboardStatsDTO.java
@@ -1,2 +1,18 @@
-package com.event.ems.dto;public class DashboardStatsDTO {
+package com.event.ems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardStatsDTO {
+
+    private long totalUsers;
+    private long totalOrganizers;
+    private long totalEvents;
+    private long pendingEvents;
+    private long totalBookings;
+    private double totalRevenue;
 }
