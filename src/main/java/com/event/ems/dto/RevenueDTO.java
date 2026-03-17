1f02b342df6dc7b4d2a7ebe7fd3ef03c04b0421d
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-03-17 09:51:37 +0530

    update: RevenueDTO

--- src/main/java/com/event/ems/dto/RevenueDTO.java
+++ src/main/java/com/event/ems/dto/RevenueDTO.java
@@ -1,2 +1,14 @@
-package com.event.ems.dto;public class RevenueDTO {
+package com.event.ems.dto;
+
+public class RevenueDTO {
+
+    private double totalRevenue;
+
+    public RevenueDTO(double totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
 }
