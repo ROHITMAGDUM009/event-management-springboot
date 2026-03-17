32771d643e437ab078abe7baf580710931c53977
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-03-17 09:51:48 +0530

    update: AdminService

--- src/main/java/com/event/ems/service/AdminService.java
+++ src/main/java/com/event/ems/service/AdminService.java
@@ -1,2 +1,18 @@
-package com.event.ems.service;public class AdminService {
+package com.event.ems.service;
+
+import com.event.ems.dto.DashboardStatsDTO;
+import com.event.ems.dto.EventRequest;
+import com.event.ems.entity.Event;
+
+import java.util.List;
+
+public interface AdminService {
+
+    // Dashboard
+    DashboardStatsDTO getDashboardStats();
+
+    // Event approvals
+    Event approveEvent(Long eventId);
+    Event rejectEvent(Long eventId);
+    List<Event> getAllEvents();
 }
