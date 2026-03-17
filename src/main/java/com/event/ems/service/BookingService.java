ba6331f1ca307fb5295585f4977778bce282ddf7
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-03-17 09:51:52 +0530

    update: BookingService

--- src/main/java/com/event/ems/service/BookingService.java
+++ src/main/java/com/event/ems/service/BookingService.java
@@ -3,8 +3,12 @@ package com.event.ems.service;
 import com.event.ems.dto.BookingRequest;
 import com.event.ems.entity.Booking;
 
+import java.util.List;
+
 public interface BookingService {
 
     Booking createBooking(BookingRequest request, String userEmail);
 
+    // ✅ NEW
+    List<Booking> getMyBookings(String userEmail);
 }
