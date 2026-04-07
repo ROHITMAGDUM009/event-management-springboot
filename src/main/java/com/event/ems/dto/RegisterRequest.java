e0703ec64ba5dfa1daceffd6a737f19d93e02ac3
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-04-07 18:17:49 +0530

    update: RegisterRequest

--- src/main/java/com/event/ems/dto/RegisterRequest.java
+++ src/main/java/com/event/ems/dto/RegisterRequest.java
@@ -1,11 +1,23 @@
 package com.event.ems.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterRequest {
 
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a valid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
-    private String role; // USER or ORGANIZER
+
+    private String role;
 
     public String getFullName() {
         return fullName;
