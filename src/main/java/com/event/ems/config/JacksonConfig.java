3eb6c1232fc5b99bd7d787659789046c7a7fdb4e
ROHITMAGDUM009 <rohitmagdum2@gmail.com>
2026-04-30 16:32:56 +0530

    chore: update JacksonConfig configuration

--- src/main/java/com/event/ems/config/JacksonConfig.java
+++ src/main/java/com/event/ems/config/JacksonConfig.java
@@ -1,4 +1,25 @@
 package com.event.ems.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
 public class JacksonConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Register Java 8 date/time module
+        mapper.registerModule(new JavaTimeModule());
+
+        // Disable writing dates as timestamps (use ISO format strings)
+        mapper.disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
+    }
 }
